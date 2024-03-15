0d06c248402c0e47f21d5b045b878b462e4f37ed
Venkatesh-vis <venkateshvishwanadula257@gmail.com>
2024-03-15 18:40:12 +0530

    js practice

---
+++ march15.js
@@ -0,0 +1,28 @@
+//Write a JavaScript function to cast the square root of a number to an integer.
+// function sqr(a){
+//      b = Math.sqrt(a)
+//      console.log(b);
+// }
+// sqr(81)
+
+
+//Write a JavaScript function to generate a random integer.
+
+// function ran(){
+//     a = Math.floor(Math.random() * 10)
+//     console.log(a);
+// }
+// ran()
+
+
+
+//Write a JavaScript function to format a number up to specified decimal places.
+
+// function format(a,b){
+//     c=a.toFixed(b)
+//     console.log(c);
+// }
+// format(2.45874,2)
+// format(9.347854,4)
+
+
---
+++ march15ii.js
@@ -0,0 +1,29 @@
+
+// var c = "ki_r_a_n"   //ouput:kiRAN
+// d = ""
+// for(i=0;i<c.length;i++){
+//     if(c[i]=="_"){
+//         d+=c[i+1].toUpperCase()
+//         i++
+//     }
+//     else{
+//         d+=c[i]
+//     }
+// }
+// console.log(d);
+
+
+
+// var a = "Ch_ai_ta_nya"  //output:ChAiTaNya
+// result = ""
+
+// for(i=0;i<a.length;i++){
+//     if(a[i]=="_"){
+//         result+=a[i+1].toUpperCase()
+//         i++
+//     }
+//     else{
+//         result+=a[i]
+//     }
+// }
+// console.log(result);
